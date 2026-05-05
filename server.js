d2c9ee3d34618fac67dee309997f70495ea3fdaa
jenkinscghs <35381477+jenkinscghs@users.noreply.github.com>
2026-05-05 12:55:34 -0400

    enable CIRS for feed polling

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ import fetch from "node-fetch";
 const app = express();
 app.use(express.json());
 
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+
 const PORT = process.env.PORT || 3000;
 
 const FEED_URL =
